cfc490db3a4843bc53ead74a078457e2bed2a0d5
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-12 19:58:56 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   ret_hash = {}
   data.each do |attributes_keys, attribute_hash|
@@ -5,6 +6,7 @@ def nyc_pigeon_organizer(data)
       name_array.each do |name|
         if !ret_hash.has_key?(name)
           ret_hash[name] = {attributes_keys => [specifics.to_s]}
+          binding.pry
         elsif !ret_hash[name].has_key?(attributes_keys)
           ret_hash[name][attributes_keys] = specifics.to_s
         end
