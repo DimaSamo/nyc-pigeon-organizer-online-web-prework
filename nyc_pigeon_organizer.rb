561c148331258302e028fce3433904ccb7d6365f
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-12 20:04:29 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,13 +7,10 @@ def nyc_pigeon_organizer(data)
         if !ret_hash.has_key?(name)
           ret_hash[name] = {attributes_keys => [specifics.to_s]}
           ##binding.pry
-        elsif !ret_hash[name].has_key?(attributes_keys)
-          ret_hash[name][attributes_keys].push(specifics.to_s)
+
         end
       end
     end
   end
   ret_hash
 end
-
-
